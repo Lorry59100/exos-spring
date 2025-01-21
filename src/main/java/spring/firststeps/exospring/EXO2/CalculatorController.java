e880192c8d6263227e6a8453d2d8ff91bb5c7f04
pisha <pisha@hotmail.fr>
2025-01-21 09:03:48 +0100

    [FIX] - multiply function added

--- src/main/java/spring/firststeps/exospring/EXO2/CalculatorController.java
+++ src/main/java/spring/firststeps/exospring/EXO2/CalculatorController.java
@@ -1,6 +1,7 @@
 package spring.firststeps.exospring.EXO2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,10 @@ public class CalculatorController {
     public String addition(@RequestParam(required = false) int a, int b) {
         return "Résultat : " + (a+b);
     }
+
+    @GetMapping("/multiply/{a}/{b}")
+    public String multiply(@PathVariable int a, @PathVariable int b) {
+        return "Résultat : " + (a*b);
+    }
+
 }
