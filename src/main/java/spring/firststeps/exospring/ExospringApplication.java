b387d29ff924396fa7f787ce96b3680d605010a0
pisha <pisha@hotmail.fr>
2025-01-21 08:37:00 +0100

    [INIT] - first init

---
+++ src/main/java/spring/firststeps/exospring/ExospringApplication.java
@@ -0,0 +1,13 @@
+package spring.firststeps.exospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExospringApplication.class, args);
+	}
+
+}
